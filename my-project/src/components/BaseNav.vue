d06083cbb609aa715f304f127151eebf653ddf64
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-01-05 16:28:47 +0700

    Create BaseNav.vue

---
+++ my-project/src/components/BaseNav.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+<template>
+<div class=" bg-gray-400 h-[80%]">
+    <div class="flex w-fit m-auto">
+        <h1 class="">hello</h1>
+        <h1 class="">hello</h1>
+    </div>
+    
+
+</div>
+</template>
+<style scoped>
+
+</style>
