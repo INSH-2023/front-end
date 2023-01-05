5e38be077d852aae2bae8463ad2832106241f7f3
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-01-05 16:28:50 +0700

    Update App.vue

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,39 +1,24 @@
 <script setup>
-import BaseNave from './components/BaseNave.vue';
+import BaseNav from './components/BaseNav.vue';
 import ShowAllEvent from './view/admin__page/ShowAllEvent.vue';
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
- 
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+<div class="h-screen w-screen">
 
-  <div class="w-[100%] h-[100%]">
-    <BaseNave/>     
+  <nav class="h-[20%]">
+    <BaseNav/>
+  </nav>
+  <div class="h-[80%]">
     <ShowAllEvent/>
-    
- 
-    
   </div>
+    
+    
+   
+</div>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
