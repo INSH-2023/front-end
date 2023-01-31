e9f540eaed91c6a3da8c5b48106f60157619a22e
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-01-31 16:16:46 +0700

    loading component

---
+++ my-project/src/components/BaseLoading.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref,onBeforeMount} from 'vue'
+
+const arrayy=ref(['.'])
+const wait =()=>{
+    if(arrayy.value.length<4){
+        arrayy.value.push('.')
+    }else 
+    if(arrayy.value.length==4){
+        arrayy.value=[]
+    }
+ 
+}
+onBeforeMount(()=>{
+    setInterval(wait,500)
+})
+</script>
+<template>
+    <div class="flex m-auto">
+        <div>
+            Waiting            
+        </div>
+        <div v-for="(d,index) in arrayy" :key="index">
+            {{d}}
+        </div>
+
+    </div>
+</template>
+<style scoped>
+</style>
