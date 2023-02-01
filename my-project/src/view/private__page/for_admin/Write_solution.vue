fd0e885aa451136d697e969945eec04acd3414ef
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-02-01 16:37:27 +0700

    add 🧑‍🦲write solution page

--- my-project/src/view/private__page/for_admin/Write_solution.vue
+++ my-project/src/view/private__page/for_admin/Write_solution.vue
@@ -1,7 +1,32 @@
 <script setup>
+import {ref} from 'vue' 
+const solutions =ref([])
+
 </script>
 <template>
-    this is for write solution
+<div class="overflow-y-auto ">
+    <div>
+        <div>
+            <!-- header -->
+            <div class="w-full text-center font-semibold text-[40px] pt-6">
+                <div class="flex w-fit mx-auto">
+                    <img src="../../../assets/admin_page/new-document.png" alt="users_icon" class="w-[50px] h-[50px] mr-4 my-auto">
+                    Write solutions
+                </div>
+                <hr class="mt-3 bg-gray-700  w-[1170px] h-[3px]">
+            </div>
+
+            <!-- body -->
+            <div class="">
+                <div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
 </template>
 <style scoped>
 </style>
