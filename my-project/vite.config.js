0c131d8ceb4a243b3c471574d536c434739887a7
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-02-20 08:12:48 +0700

    learning : env :
    - ศึกษาเกี่ยวกับ environment ของในการ deploy

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolveComponent } from 'vue';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // base:`/kw1/`,
+  server: {
+    proxy: {
+      // Focus here
+      '/api':{
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => {
+          console.log(path);
+          return path.replace('/^\/api/','') }
+        }
+      }
+    }
 })
