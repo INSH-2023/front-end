b4ca70cd3e891956f47f6109e6da9387286721cf
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-02-27 16:36:15 +0700

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolveComponent } from 'vue';
+import path from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'./src')
+    }
+  },
   // base:`/kw1/`,
   server: {
 
     proxy: {
       '/api': {
-           target: 'https://localhost:172.17.0.2',
+           target: 'http://10.1.1.167:5000',
            changeOrigin: true,
            secure: false,      
            ws: true,
