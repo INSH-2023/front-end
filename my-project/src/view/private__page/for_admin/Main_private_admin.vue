dce60818127de797fedcb33b4adc8730cf9d244f
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-03-28 16:23:06 +0700

    edit : main_private_admin :
    - ปรุบปรุงให้สามารถใช้ต้อง login ก่อนถึงจะใช้งาน feature ต่างๆได้

--- my-project/src/view/private__page/for_admin/Main_private_admin.vue
+++ my-project/src/view/private__page/for_admin/Main_private_admin.vue
@@ -1,16 +1,32 @@
 <script setup>
+import {ref, onBeforeMount,onMounted } from 'vue';
 import BaseLeftBar from '../../../components/BaseLeftBar.vue';
 import BaseNav from '../../../components/BaseNav.vue';
+import validate from '../../../JS/validate';
+
 // import ShowAllEvent from './for_admin/ShowAllEvent.vue';
+const role=ref(undefined)
+const userName=ref(undefined)
+onBeforeMount(()=>{
+   role.value = validate.getUserDataFromLocal('user_role')
+   userName.value = validate.getUserDataFromLocal('user_email')
+})
+
+onMounted(()=>{
+   validate.vLogin()
+})
+
+
 </script>
 <template>
  
- <BaseNav  class="h-[9.5%]  w-full"/>
+ <BaseNav :role="role" :user-name="userName" class="h-[9.5%]  w-full"/>
  <div class="flex h-[90.5%]">
     <BaseLeftBar class="w-[20vw] "/>
     <router-view class="w-[80vw]" />  
  </div>
   
+ <!-- <button @click="myRouter.push({name:'signIn'})">sdfsdfsdf</button> -->
 
 </template>
 <style scoped>
