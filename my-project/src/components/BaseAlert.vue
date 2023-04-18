45dc0a34a1cb38f5e3bc1bc1d3b0673565a32e10
Sahathat Yingsakulkiet <72536750+Phiraphat0123@users.noreply.github.com>
2023-04-18 16:08:25 +0700

    edit : BaseAlert :
    - ปรับปรุงให้สามารถ alert เป็นข้อมูลต่างๆตามหน้า page

--- my-project/src/components/BaseAlert.vue
+++ my-project/src/components/BaseAlert.vue
@@ -13,35 +13,74 @@ const props =defineProps({
         type:String,
         default:'',
         // required:true
+    },
+    type:{
+        type:String,
+        default:'login',
+
     }
 })
 
 </script>
 <template>
+    <!-- login -->
+    <div v-if="props.type =='login'" >
     <!-- waring -->
-    <div v-if="props.status==false" class="absolute top-10 right-0 w-[300px] h-fit p-3 bg-[#fcbf49] border-l-2 border-gray-500">
-        <!-- header -->
-        <h4 class="font-semibold text-[16px]">
-            {{props.title}}
-        </h4>
-        <!-- article -->
-        <h5 class="font-light text-[14px]">
-            {{ props.message }}
-        </h5>
-        <button class="absolute top-1 right-2">
-            X
-        </button>
+        <div v-if="props.status==false" >
+            <div class="relative hidden top-10 right-0 w-[300px] h-fit p-3 bg-[#fcbf49] border-l-2 border-gray-500 lg:block lg:absolute">
+                <h4 class="font-semibold text-[16px]">
+                    {{props.title}}
+                </h4>
+                <!-- article -->
+                <h5 class="font-light text-[14px]">
+                    {{ props.message }}
+                </h5>
+            </div>
+            <div class="relative block  w-[300px] h-fit mx-auto mt-2 mb-3 p-3 border-2 border-[#fcbf49] rounded sm:w-[400px] lg:hidden lg:hidden">
+                <h4 class="font-semibold text-[16px] text-[#fcbf49]">
+                    {{props.title}}
+                </h4>
+                <!-- article -->
+                <h5 class="font-light text-[14px] text-[#fcbf49]">
+                    {{ props.message }}
+                </h5>
+            </div>
+            <!-- header -->
+            
+            <!-- <button @click="props.status=undefined" class="absolute top-1 right-2">
+                X
+            </button> -->
+        </div>
+        <!-- error -->
+        <div v-else-if="props.status==true" >
+            <div class="relative hidden top-10 right-0 w-[300px] h-fit p-3 bg-[#ef233c] border-l-2 border-gray-500 lg:block lg:absolute">
+                <h4 class="font-semibold text-[16px]">
+                    {{props.title}}
+                </h4>
+                <!-- article -->
+                <h5 class="font-light text-[14px]">
+                    {{ props.message }}
+                </h5>
+            </div>
+            <div class="relative block  w-[300px] h-fit mx-auto mt-2 mb-3 p-3 border-2 border-[#ef233c] rounded sm:w-[400px] lg:hidden lg:hidden">
+                <h4 class="font-semibold text-[16px] text-[#ef233c]">
+                    {{props.title}}
+                </h4>
+                <!-- article -->
+                <h5 class="font-light text-[14px] text-[#ef233c]">
+                    {{ props.message }}
+                </h5>
+            </div>
+        </div>
+
+
     </div>
-    <!-- error -->
-    <div v-else-if="props.status==true" class="absolute top-10 right-0 w-[300px] h-fit p-3 bg-[#ef233c] border-l-2 border-gray-500">
-        <h4 class="font-semibold text-[16px]">
-            {{props.title}}
-        </h4>
-        <!-- article -->
-        <h5 class="font-light text-[14px]">
-            {{ props.message }}
-        </h5>
+    <div v-else>
+        
     </div>
+
+    
+    
 </template>
 <style>
 
