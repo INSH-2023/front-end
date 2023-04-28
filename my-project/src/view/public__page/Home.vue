f74de52ad9494520ecff28ca7a958992c660176f
Sahathat Yingsakulkiet <72536750+Phiraphat0123@users.noreply.github.com>
2023-04-28 15:42:22 +0700

    commentOut

--- my-project/src/view/public__page/Home.vue
+++ my-project/src/view/public__page/Home.vue
@@ -30,7 +30,7 @@ const goSignIn=()=>myRouter.push({name:'signIn'})
           md:text-[40px] sm:text-[20px] 
           md:block md:ml-[2.6rem] md:w-full md:text-left md:text-[1.3rem] lg:text-[2.5rem]
       ">
-         <!-- เว็บไซต์การให้บริการสำหรับแจ้งปัญหาด้าน IT ต่างๆ -->
+         เว็บไซต์การให้บริการสำหรับแจ้งปัญหาด้าน IT ต่างๆ
         <span class="w-fit  text-[0.938rem]  sm:text-[1.25rem] md:text-[1.62rem] lg:text-[2.3rem]">คุณมีปัญหาที่เกี่ยวกับไอทีใช่หรือไม่?</span>  <br>ลองใช้เว็บไซต์นี้ดูสิ
       </div>
       <div class="w-[14.375rem] m-auto my-2 mt-auto text-[0.75rem] text-center font-normal w-full font-medium
@@ -49,7 +49,7 @@ const goSignIn=()=>myRouter.push({name:'signIn'})
          </button>
       </div>
    </div>
-   <!-- photo -->
+
    <div class=" w-full   mx-auto  z-0 w-[16rem] order-2
       sm:relative  sm:w-[16rem] sm:top-auto 
       md:w-[25rem] md:m-auto
@@ -57,6 +57,75 @@ const goSignIn=()=>myRouter.push({name:'signIn'})
       <img src="../../assets/contract_1.jpg" alt="photo" class="mx-auto">
    </div>
 </div>
+<!-- <div style='text-align:center;width:100dvw;height: fit-content;background-color: gray;padding: 16px;'>
+      <div style='width:100%;background-color: pink;font-size:15px'>        
+      
+         <div style="=width:90%;margin:auto;">
+            <div style="width:fit-content;text-align: left;">
+
+               <h2 style='display: inline-block;color:#219ebc;text-align: left;font-size: 32px;'>Hello </h2>
+               <h4 style='display: inline-block;text-align: left;margin-left:20px;margin-top:2px;font-size: 23px;font-weight: lighter;'>to@mail.com </h4>
+               <hr style="width: 70%;border-top: 2px solid gray">
+            </div> 
+            <h5 style='width:fit-content;margin:auto;margin-top:10px;margin-bottom:10px;font-size: 23px;font-weight: bold;'>คำร้องของคุณ</h5>
+
+            <hr style="width: 60%;margin:auto;border-top: 2px solid gray;">
+            <table style="width:fit-content;margin:15px auto 15px auto;">
+               <tr>
+                  <th style="text-align: right;">
+                     ประเภทของอุปกรณ์ :
+                  </th>
+                  <td style="text-align: left;">
+                     Notebook
+                  </td>
+               </tr>
+               <tr >
+                  <th style="text-align: right;">
+                     ประเภทการใช้งาน :
+                  </th>
+                  <td style="text-align: left;">
+                     เป็นเครื่องขององค์กรณ์
+                  </td>
+               </tr>
+               <tr>
+                  <th style="text-align: right;">
+                     ยี่ห้อ :
+                  </th>
+                  <td style="text-align: left;">
+                     เครื่องโหดมากๆ
+                  </td>
+               </tr>
+            </table>
+            <hr style="width: 60%;margin:auto;border-top: 2px solid gray">
+            <div style="width:fit-content;margin-top: 15px;margin:auto;text-align: left;">
+                  <div style="margin:10px 15px 0px 15px;">
+                     <h5 style="display: block;text-align: left;font-weight: bold;">
+                        ปัญหาที่พบ :
+                     </h5>
+                     <p style="text-align: left;margin-left:20px">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse excepturi impedit tenetur, id quod amet.
+                     </p>
+                  </div>
+                  <div style="margin:10px 15px 0px 15px;">
+                     <h5 style="display: block;text-align: left;font-weight: bold;">
+                        ปัญหาอื่นๆ ที่พบเจอ :
+                     </h5>
+                     <p style="text-align: left;margin-left:20px">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse excepturi impedit tenetur, id quod amet.
+                     </p>
+                  </div>
+                  <div style="margin:10px 15px 0px 15px;">
+                     <h5 style="display: block;text-align: left;font-weight: bold;">
+                        เพิ่มเติม :
+                     </h5>
+                     <p style="text-align: left;margin-left:20px">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse excepturi impedit tenetur, id quod amet.
+                     </p>
+                  </div>
+            </div>
+         </div>
+      </div> 
+</div> -->
 </template>
 <style scoped>
 </style>
