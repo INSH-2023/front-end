96eac27c74459d37e47a87ea33fcd4b13151763d
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-07-12 15:10:59 +0700

    fix bug make web crash

--- my-project/src/view/private__page/for_user/Booking_main.vue
+++ my-project/src/view/private__page/for_user/Booking_main.vue
@@ -68,7 +68,7 @@ const findingKeyW = async (keyWord) => {
         //         console.log('this from searching step: ', matchStep(solu.solution_steps, keyWord))
         //     }
     }
-    if (arr.length != 0) {
+    if (arr.length != 0 && solutionList.length >= 4 ) {
         await randomArticle(4, arr.length, arr)
     }
 }
@@ -438,7 +438,7 @@ onBeforeMount(() => {
     <hr class="hidden mt-[30px] w-[1100px] h-[1px] border-0 mx-auto bg-gray-700 lg:block">
 
     <!-- searching -->
-    <div v-if="params.service == 'it' && solutionList.length != 0" class="relative w-[600px] h-[150px] mx-auto">
+    <div v-if="params.service == 'it' && solutionList.length >= 4" class="relative w-[600px] h-[150px] mx-auto">
         <h3 class="mx-auto w-fit mt-4 text-[30px]">
             หรือค้นหาหัวข้อปัญหาที่คุณเจอ
         </h3>
