773ef939d3a6d6dd71916d70a19ac5958d013b30
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-07-20 16:26:13 +0700

    fix bug power bi cannot public show

--- my-project/src/view/private__page/for_admin/Dashboard.vue
+++ my-project/src/view/private__page/for_admin/Dashboard.vue
@@ -3,7 +3,7 @@
 </script>
  
 <template>
-<iframe title="IT_help_desk" width="100%" height="100%" src="https://app.powerbi.com/reportEmbed?reportId=9f103620-eb9d-4453-9b61-94681eb01356&autoAuth=true&ctid=6f4432dc-20d2-441d-b1db-ac3380ba633d" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="IT_help_desk" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiODM1NmJkMDMtYTVjYy00YzBhLTk0ZDYtY2JmYmQ2NDJmMTE5IiwidCI6IjZmNDQzMmRjLTIwZDItNDQxZC1iMWRiLWFjMzM4MGJhNjMzZCIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 </template>
  
 <style>
