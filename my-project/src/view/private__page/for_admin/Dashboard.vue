88f745dd3a80984e93a62cb6719622b02d9be401
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-07-20 15:30:35 +0700

    make dashboard power bi support

---
+++ my-project/src/view/private__page/for_admin/Dashboard.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+ 
+<template>
+<iframe title="IT_help_desk" width="100%" height="100%" src="https://app.powerbi.com/reportEmbed?reportId=9f103620-eb9d-4453-9b61-94681eb01356&autoAuth=true&ctid=6f4432dc-20d2-441d-b1db-ac3380ba633d" frameborder="0" allowFullScreen="true"></iframe>
+</template>
+ 
+<style>
+
+</style>
