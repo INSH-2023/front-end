51880883418b1cbd41693cca3455620bf6f41626
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-07-21 09:01:13 +0700

    update dashboard

--- my-project/src/view/private__page/for_admin/Dashboard.vue
+++ my-project/src/view/private__page/for_admin/Dashboard.vue
@@ -3,7 +3,7 @@
 </script>
  
 <template>
-<iframe title="IT_help_desk" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiODM1NmJkMDMtYTVjYy00YzBhLTk0ZDYtY2JmYmQ2NDJmMTE5IiwidCI6IjZmNDQzMmRjLTIwZDItNDQxZC1iMWRiLWFjMzM4MGJhNjMzZCIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="Report Section" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiYTI2M2M0NGUtZmJkMi00MzQ1LTkxM2UtNTA5YTlkMDI5YTJmIiwidCI6IjZmNDQzMmRjLTIwZDItNDQxZC1iMWRiLWFjMzM4MGJhNjMzZCIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 </template>
  
 <style>
